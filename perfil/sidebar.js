d1891bb6d4ce244488c856e22e4745d3a7f3fbdb
Willian Teotonio <willianpersonona@gmail.com>
2024-05-23 15:51:03 -0300

    add perfil to main

---
+++ perfil/sidebar.js
@@ -0,0 +1,17 @@
+//Efeito SiderBar-Open
+document.getElementById('open_btn').addEventListener('click', function () {
+    document.getElementById('sidebar').classList.toggle('open-sidebar');
+});
+
+//Alert box save e cancel
+function salvar() {
+    alert("Tudo foi salvo com sucesso!");
+}
+
+function cancelar() {
+    if (confirm("Tem certeza que deseja cancelar?")) {
+        alert("Tudo foi cancelado com sucesso!");
+    }
+}
+
+
